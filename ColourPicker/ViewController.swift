d54a7c986d65422a1767044d1bf9be7d60bc92cd
Colin Hill <colin@peak2peakmedia.com>
2017-01-24 13:05:09 +0000

    Initial Commit

---
+++ ColourPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColourPicker
+//
+//  Created by Colin Hill on 1/24/17.
+//  Copyright © 2017 Colin Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
